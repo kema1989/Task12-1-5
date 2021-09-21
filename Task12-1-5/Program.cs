609554dafd8727810ea988b5cf1d95fc251913f8
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-09-22 00:01:50 +0600

    Добавьте файлы проекта.

---
+++ Task12-1-5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Task12_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new List<User>();
+            users.Add(new User("lex74", "Алексей Демиденко", false));
+            users.Add(new User("", "Екатерина Колесникова", true));
+            users.Add(new User("mike01", "Mike Peterson", true));
+            users.Add(new User("bomj99", "Бомж Бомжов", false));
+            users.Add(new User("?????", "Аноним", true));
+
+            Console.WriteLine("Введите логин:");
+            string login = Console.ReadLine();
+            for (int i = 0; i < users.Count; i++)
+            {
+                if (login == users[i].Login)
+                {
+                    if (users[i].IsPremium == true)
+                    {
+                        Console.WriteLine($"Здравствуйте, {users[i].Name}, как поживаете?)))");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"А, халявщег? На вот, рекламку глянь)");
+                        ShowAds();
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Такого пользователя нет...");
+                }
+            }
+        }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ Task12-1-5/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12_1_5
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User(string login, string name, bool isPremium)
+        {
+            Login = login;
+            Name = name;
+            IsPremium = isPremium;
+        }
+    }
+}
